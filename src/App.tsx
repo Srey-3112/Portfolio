429141e69a08e39778d60d2180a0bd7f7cdc6d38
Srey-3112 <sreypatel3112@gmail.com>
2025-06-09 03:29:12 +0530

    app.tsx fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,43 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  // Handle GitHub Pages routing
+  useEffect(() => {
+    if (window.location.search.startsWith('?/')) {
+      const pathSegmentsToKeep = 1;
+      const path = window.location.search.slice(2).replace(/&/g, '/').replace(/~and~/g, '&');
+      window.history.replaceState(
+        null,
+        '',
+        window.location.pathname.slice(0, window.location.pathname.indexOf('/', 1) + 1) + 
+        path + 
+        window.location.hash
+      );
+    }
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter basename="/Portfolio">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
